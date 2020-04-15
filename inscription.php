65bc7da510df1e3178a0ae006cbcbe8ca32a3a4c
Axel-LAPTOP <axel.abad0901@gmail.com>
2020-04-15 12:04:50 +0200

    light MAJ Inscription

--- inscription.php
+++ inscription.php
@@ -1,7 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les archives</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="icon" href="images/holocron-icon.png">
+</head>
+<body>
+<main>
 <?php 
+include ('includes/header.php');
 
-if (!empty($_POST['info_prenom']) && !empty($_POST['info_password']) && !empty($_POST['info_email']) && !empty($_POST['info_password'])) {
-
+if (!empty($_POST['user_prenom']) && !empty($_POST['info_password']) && !empty($_POST['user_email']) && !empty($_POST['user_password'])) {
+user
 	try{
 		# première étape : je me connecte au serveur
 		$sqlInsc = new PDO("mysql:host=localhost;dbname=entrainementphpbdd", "root");
@@ -10,10 +23,10 @@ if (!empty($_POST['info_prenom']) && !empty($_POST['info_password']) && !empty($
 		$stmt = $sqlInsc->prepare("INSERT INTO `info` (`info_id`, `info_prenom`, `info_nom`, `info_mail`, `info_password`) VALUES (NULL, :info_prenom, :info_nom, :info_email, :info_password);");
 		
 	// je lui donne les paramètres dont elle a besoin sans en oublier
-		$stmt->bindValue(":info_prenom", $_POST['info_prenom']);
-		$stmt->bindValue(":info_nom", $_POST['info_nom']);
-		$stmt->bindValue(":info_email", $_POST['info_email']);
-		$stmt->bindValue(":info_password", password_hash($_POST['info_password'], PASSWORD_DEFAULT));
+		$stmt->bindValue(":user_prenom", $_POST['user_prenom']);
+		$stmt->bindValue(":user_nom", $_POST['user_nom']);
+		$stmt->bindValue(":user_email", $_POST['user_email']);
+		$stmt->bindValue(":user_password", password_hash($_POST['user_password'], PASSWORD_DEFAULT));
 
 		// Je l'execute et en fonction de si l'email existe deja ou pas, j'insere ma requete dans la bdd
 		$stmt->execute();
@@ -31,3 +44,11 @@ if (!empty($_POST['info_prenom']) && !empty($_POST['info_password']) && !empty($
 			echo $exception->getMessage();
 	}
 }
+?>
+</main>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="scripts/script.js"></script>
+</body>
+</html>
